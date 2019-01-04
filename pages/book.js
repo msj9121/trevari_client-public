46268c4966e4bd155606f91d824251dd6b0f7987
Sung Jin, Moon <msj9121@gmail.com>
2019-01-04 16:37:00 +0900

    add bookmark && delete bookmark (_app.js_state-Change X)

--- pages/book.js
+++ pages/book.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import fetch from 'isomorphic-unfetch';
 import axios from 'axios';
 
 class Book extends Component {
@@ -10,40 +9,98 @@ class Book extends Component {
   static async getInitialProps(context) {
     const { id } = context.query
     const res = await axios.post(`http://3.16.58.104:5000/books/getBookById`, { id })
-    const show = await res.data
+    const book = await res.data
 
-    return { show }
+    return { book }
   }
 
-  _addBookmark = () => {
-    if(this.props.ID) {
-      console.log("북마크 : ", this.props.show.id)
-      alert("북마크 POST 요청")
+  _addBookmark = async () => {
+    if (this.props.ID) {
+      const { userId, bookId } = {
+        userId: this.props.ID,
+        bookId: this.props.book.id
+      }
+      const res = await axios.post(`http://3.16.58.104:5000/bookmarks/addBookmark`, { userId, bookId })
+      const data = await res.data
+      console.log("POST-addBookmark response : ", data)
+      alert("addBookmark POST 요청")
+
+      /* --------------------------------------App state change--------------------------------------------------------- */
+
+      // const res = await axios.post(`http://3.16.58.104:5000/bookmarks/getMyBookmarks`, {
+      //   userId: this.props.ID
+      // })
+      // const changeBookmarkDatas = await res.data
+      // console.log("GET-changeBookmarkDatas response : ", filterBookmarkId())
+
     } else {
       alert("로그인 해주세요!")
     }
   }
 
+  _deleteBookmark = async () => {
+
+
+    if (this.props.ID) {
+      const bookId = this.props.book.id
+      const bookmarkDatas = this.props.bookMarkData
+      const filterBookmarkId = () => {
+        for(let i = 0; i < bookmarkDatas.length; i++) {
+          if(bookmarkDatas[i].book_id === bookId) {
+            return bookmarkDatas[i].id
+          }
+        }
+      }
+      
+      const bookmark_Id = filterBookmarkId()
+      console.log(bookmark_Id)
+
+      const res = await axios.post(`http://3.16.58.104:5000/bookmarks/deleteBookmark`, {
+        userId: this.props.ID,
+        bookmarkId: bookmark_Id
+      })
+
+      const data = await res.data
+      console.log("POST-deleteBookmark response : ", data)
+
+      alert("deleteBookmark POST 요청")
+      
+      /* --------------------------------------App state change--------------------------------------------------------- */
+      
+      // const res = await axios.post(`http://3.16.58.104:5000/bookmarks/getMyBookmarks`, {
+      //   userId: this.props.ID
+      // })
+      // const changeBookmarkDatas = await res.data
+      // console.log("GET-changeBookmarkDatas response : ", filterBookmarkId())
+    } 
+    else {
+      alert("로그인 해주세요!")
+    }
+
+
+  }
+
   render() {
-    console.log("Book.js--ID : ", this.props.ID)
+    console.log("Book.js--bookMarkData : ", this.props.bookMarkData)
     return (
       <div id="book">
 
         <div id="book_box">
           <div className="book_titlebox">
-            <div className="book_titlebox_img"><img src={this.props.show.image}></img></div>
+            <div className="book_titlebox_img"><img src={this.props.book.image}></img></div>
             <div className="book_titlebox_title">
-              <div className="book_titlebox_titleName">{this.props.show.title}</div>
-              <div className="book_titlebox_author">저자 : {this.props.show.author}</div>
-              <div className="book_titlebox_author">{this.props.show.publishedAt}</div>
-              <div className="book_titlebox_author">ISBN : {this.props.show.isbn}</div>
+              <div className="book_titlebox_titleName">{this.props.book.title}</div>
+              <div className="book_titlebox_author">저자 : {this.props.book.author}</div>
+              <div className="book_titlebox_author">{this.props.book.publishedAt}</div>
+              <div className="book_titlebox_author">ISBN : {this.props.book.isbn}</div>
               <div className="book_titlebox_grade">평점 ★★★★☆ 8(210명)</div>
-              <span className="book_titlebox_bookmarkBtn" onClick={this._addBookmark}>+ 읽고싶어요</span>
+              <span className="book_titlebox_addBookmarkBtn" onClick={this._addBookmark}>+ 읽고싶어요</span>
+              <span className="book_titlebox_addBookmarkBtn" onClick={this._deleteBookmark}>- 읽고싶어요</span>
               <span className="book_titlebox_gradeBtn">+ 평점주기</span>
             </div>
           </div>
 
-          <p>{this.props.show.description}</p>
+          <p>{this.props.book.description}</p>
         </div>
 
         <style jsx>{`
@@ -85,7 +142,7 @@ class Book extends Component {
             font-size: 25px;
             margin-bottom: 20px;
           }
-          .book_titlebox_bookmarkBtn {
+          .book_titlebox_addBookmarkBtn {
             border: 1px solid #DDD;
             font-size: 20px;
             padding: 5px 15px 5px 15px;
