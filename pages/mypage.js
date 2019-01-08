02d3683d3e0f66cf380889e7d04c6c3ba706f844
Anne A <aaguirre.rdit@gmail.com>
2019-01-08 10:17:17 +0900

    finished modal window

--- pages/mypage.js
+++ pages/mypage.js
@@ -1,33 +1,34 @@
 import axios from "axios";
 import MypageContents from "../components/mypage/MypageContents";
-
+import UpdateUserData from '../components/mypage/UpdateUserData';
+import React from "react";
 
 
 class Mypage extends React.Component {
-  
+
   static getInitialProps = async function (context) {
     const { userId } = context.query
 
     if (userId) {
       console.log("[*] mypage props.ID : ", { userId })
-  
+
       const url1 = 'http://localhost:5000/bookmarks/getMyBookmarks'
       const realAPI1 = 'http://3.16.58.104:5000/bookmarks/getMyBookmarks'
       // app.state.bookmark의 데이터를 가져오도록 수정하야야 한다.
-      
+
       const res1 = await axios.post(realAPI1, { userId })
         .catch(err => console.log(err))
       const books = await res1.data;
       // console.log("[*] books : ", books);
-  
+
       const url2 = 'http://localhost:5000/reviews/getMyReviews'
       const realAPI2 = 'http://3.16.58.104:5000/reviews/getMyReviews'
-      
+
       const res2 = await axios.post(realAPI2, { userId })
         .catch(err => console.log(err))
       const reviews = await res2.data;
       // console.log("[*] reviews : ", reviews);
-  
+
       return {
         books: books,
         reviews: reviews
@@ -45,12 +46,15 @@ class Mypage extends React.Component {
       reviews: this.props.reviews,
       ratedBooksShow: true,
       wantToReadBooksShow: false,
-    }
+      userDataModal:false
+    };
     console.log("[*] mypage this.state.id : ", this.state.id);
-    
+
     this.ratedBooks_clickHandler = this.ratedBooks_clickHandler.bind(this)
     this.wantToReadBooks_clickHandler = this.wantToReadBooks_clickHandler.bind(this)
-    this.getBooks = this.getBooks.bind(this)
+    this.getBooks = this.getBooks.bind(this);
+    this.openUserDataModal = this.openUserDataModal.bind(this);
+    this.closeUserDataModal = this.closeUserDataModal.bind(this);
   }
 
   render () {
@@ -71,7 +75,12 @@ class Mypage extends React.Component {
     } else {
       return (
         <div id="mypage">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+                  integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+                  crossOrigin="anonymous"></link>
+            {this.state.userDataModal ? <UpdateUserData userId ={this.props.ID} onclose={this.closeUserDataModal}/> : undefined}
           <div id="mypage_box">
+              <button id={'userSettingsButton'} onClick={this.openUserDataModal}><i className="fas fa-cog"></i></button>
             <h1>마이페이지</h1>
           </div>
           <div id="Mypage_nav">
@@ -94,6 +103,14 @@ class Mypage extends React.Component {
             </div>
           </div>
           <style jsx>{`
+              #userSettingsButton{
+                float:right;
+                margin-top:30px;
+                margin-right:10px;
+                font-size:16px;
+                color:grey;
+                border-radius:10%;
+              }
               #test {
                 border: solid 2px;
               }
@@ -113,7 +130,7 @@ class Mypage extends React.Component {
                 }
               }
               #Mypage_nav {
-      
+
                 border: solid 1px #ced4da;
                 margin: 0 auto;
                 width: 60%;
@@ -203,11 +220,11 @@ class Mypage extends React.Component {
 
   getBooks = async () => {
     const userId = { userId: 1 }
-    // const userId = { userId: this.props.ID } 
+    // const userId = { userId: this.props.ID }
 
     const url2 = 'http://localhost:5000/reviews/getMyReviews'
     const realAPI2 = 'http://3.16.58.104:5000/reviews/getMyReviews'
-    
+
     const res2 = await axios.post(realAPI2, userId)
       .catch(err => console.log(err))
     const reviews = await res2.data;
@@ -219,6 +236,17 @@ class Mypage extends React.Component {
       };
     });
   };
+
+  openUserDataModal = () => {
+    this.setState({
+        userDataModal:true
+    })
+  }
+  closeUserDataModal = () => {
+    this.setState({
+        userDataModal:false
+    })
+  }
 }
 
 export default Mypage;
