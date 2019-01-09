4f3f6b68b9ee0beafd0c487c4f4c17697311951e
codeMoon <37973583+msj9121@users.noreply.github.com>
2019-01-09 13:29:01 +0900

    Update constant.js

--- constant.js
+++ constant.js
@@ -1,4 +1,4 @@
-const IS_PRODUCTION = process.env.NODE_ENV !== "production";
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
 const BACKEND_ENDPOINT = IS_PRODUCTION
   ? "http://3.16.58.104:5000"
   : "http://localhost:5000";
