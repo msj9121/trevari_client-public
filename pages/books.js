c3d17c74cd3654a751d0478e39da40461e05da09
Sung Jin, Moon <msj9121@gmail.com>
2019-01-10 10:11:29 +0900

    search add shouldComponentUpdate

--- pages/books.js
+++ pages/books.js
@@ -1,51 +1,68 @@
+import React, { Component } from "react";
 import axios from "axios";
 import BooksBanner from "../components/books/BooksBanner";
 import BooksBestsellers from "../components/books/BooksBestsellers";
 import { BACKEND_ENDPOINT } from "../constant";
 
-const Books = props => {
-  return (
-    <div>
-      <BooksBanner />
-      <div id="books">
+class Books extends Component {
+  static async getInitialProps() {
+    const res = await axios.post(`${BACKEND_ENDPOINT}/books/searchByTitle`, {
+      input: "대한"
+    });
+    const data = await res.data.slice(0, 6);
+    
+    return {
+      bestsellers: data
+    };
+  }
 
-        <div id="books_box">
+  constructor(props) {
+    super(props)
+    this.state = {
+      bestsellers: this.props.bestsellers
+    }
+  }
 
-          <BooksBestsellers bestsellers={props.bestsellers} ID={props.ID}/>
-          <BooksBestsellers bestsellers={props.bestsellers} ID={props.ID}/>
-          <BooksBestsellers bestsellers={props.bestsellers} ID={props.ID}/>
-          
-        </div>
+  render() {
+    return (
+      <div>
+        <BooksBanner />
+        <div id="books">
+          <div id="books_box">
+            <BooksBestsellers
+              bestsellers={this.state.bestsellers}
+              ID={this.props.ID}
+            />
+            <BooksBestsellers
+              bestsellers={this.state.bestsellers}
+              ID={this.props.ID}
+            />
+            <BooksBestsellers
+              bestsellers={this.state.bestsellers}
+              ID={this.props.ID}
+            />
+          </div>
 
-        <style jsx>{`
-          #books {
-            background: rgba(0, 0, 0, 0.03);
-          }
-          #books_box {
-            border: 1px solid #DDD;
-            margin: 0 auto;
-            width: 60%;
-            background-color: white;
-          }
-          @media screen and (max-width: 600px) {
-            #books_box {
-              width: 100%;
+          <style jsx>{`
+            #books {
+              background: rgba(0, 0, 0, 0.03);
             }
-          }
-        `}</style>
-
+            #books_box {
+              border: 1px solid #ddd;
+              margin: 0 auto;
+              width: 60%;
+              background-color: white;
+            }
+            @media screen and (max-width: 600px) {
+              #books_box {
+                width: 100%;
+              }
+            }
+          `}</style>
+        </div>
       </div>
-    </div>
-  );
-};
-
-Books.getInitialProps = async function () {
-  const res = await axios.post(`${BACKEND_ENDPOINT}/books/searchByTitle`, { input: "대한"});
-  const data = await res.data.slice(0, 6);
-
-  return {
-    bestsellers: data
-  };
-};
+    );
+  }
+}
 
 export default Books;
